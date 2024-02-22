6fe650090a9dfdaac9577037a9f7007dabf37828
Saif <saifrafaat99@gmail.com>
2024-02-22 11:18:49 +0200

    just started

---
+++ src/routes/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../commponents/Navbar'
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      colors: {
+        'main': '#FF4D30',
+        'primary': '#', // replace with your main color
+        // 'secondary': '#b44949', // replace with your main color
+      },
+    },
+  },
+  plugins: [],
+}
+
