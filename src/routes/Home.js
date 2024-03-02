a2e70af73255c81aa264d689adb6dd1ff9d4fe07
Saif <saifrafaat99@gmail.com>
2024-03-02 15:15:29 +0200

    Book section

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../commponents/Navbar'
 import Hero from '../commponents/Hero'
+import BookCar from '../commponents/Book/BookCar'
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Hero/>
+      <BookCar/>
     </div>
   )
 }
