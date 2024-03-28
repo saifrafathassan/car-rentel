2544d5ce282b908c7ef25d7f3756a654e1de056e
Saif <saifrafaat99@gmail.com>
2024-03-29 00:40:04 +0200

    book css

--- src/commponents/Book/BookCar.js
+++ src/commponents/Book/BookCar.js
@@ -232,7 +232,7 @@ const BookCar = () => {
                     <FcCalendar className="input-icon" /> &nbsp; Pick-up{" "}
                     <b>*</b>
                   </label>
-                  <input
+                  <input className='py-[1.2rem]'
                     id="picktime"
                     value={pickTime}
                     onChange={handlePickTime}
@@ -245,7 +245,7 @@ const BookCar = () => {
                     <FcCalendar className="input-icon" /> &nbsp; Drop-of{" "}
                     <b>*</b>
                   </label>
-                  <input
+                  <input className='py-[1.2rem]'
                     id="droptime"
                     value={dropTime}
                     onChange={handleDropTime}
